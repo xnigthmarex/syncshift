12776eb36b3f29a82ae0fc8f06918b4f2aa1855a
Aryan Singh <aryansin0448@k12.ipsd.org>
2023-09-12 21:38:29 -0500

    DONE

---
+++ next-env.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
+import { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user?: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
