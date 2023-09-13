240a29e9781a65af2acf7007d36eba84e0636f77
Aryan Singh <135762633+xnigthmarex@users.noreply.github.com>
2023-09-13 08:14:37 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
   <Providers>
-    <body className={`${inter.className} bg-black`}>
+    <body className={`${inter.className} dark`}>
       {children}
     </body>
   </Providers>
